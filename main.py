53ee7c4d32b2fcf4fd60c4124e577780017a8453
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2022-04-07 23:17:26 +0600

    fixed typo

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-""" GlobalStats GTD Update Automation Script
+""" Globalstats GTD Update Automation Script
 
    * to test the program run: $ python3 main.py test
    * monthly update: $ python3 main.py monthly
    * weekly update: $ python3 main.py weekly
 
     author: Ashraf Minhaj
-    mail  : minhaj@programming-hero.com 
+    mail  : ashraf_minhaj@yahoo.com 
 """
 
 
@@ -36,18 +36,18 @@ sign_in_url = 'https://globalstats.io/login'
 key = ''
 
 # gtd: Globalstats Tracked Data 
-weekly_gdt_key = 'weekly'               # gdt key and id are same
-weekly_gdt_short_name = 'wkl'
-monthly_gdt_key = 'monthly'
-monthly_gdt_short_name = 'mth'
+weekly_gtd_key = 'weekly'               # gdt key and id are same
+weekly_gtd_short_name = 'wkl'
+monthly_gtd_key = 'monthly'
+monthly_gtd_short_name = 'mth'
 
-test_gdt_key = 'testly'
-test_gdt_short_name = 'tst'
+test_gtd_key = 'testly'
+test_gtd_short_name = 'tst'
 
 mygames_url = 'https://globalstats.io/endpoints/' + key
-weekly_dlt_url = mygames_url + '/gtds/' + weekly_gdt_key
-monthly_dlt_url = mygames_url + '/gtds/' + monthly_gdt_key
-test_gdt_dlt_url = mygames_url + '/gtds/' + test_gdt_key
+weekly_dlt_url = mygames_url + '/gtds/' + weekly_gtd_key
+monthly_dlt_url = mygames_url + '/gtds/' + monthly_gtd_key
+test_gdt_dlt_url = mygames_url + '/gtds/' + test_gtd_key
 gtd_create_url  = mygames_url + '/gtds/create'
 
 # set up logging
@@ -59,8 +59,8 @@ logging.basicConfig(
 
 # unpack argument
 logging.info(f'Got {len(sys.argv)} vairables')
-gdt = sys.argv[1]
-logging.info(f'Operaiton for data:{gdt}')
+gtd = sys.argv[1]
+logging.info(f'Operaiton for data:{gtd}')
 
 logging.info('Initialize Virtual Display')
 display = Display(visible=0, size=(1366, 768))
@@ -111,7 +111,6 @@ def delete_gtd(url):
     # check purge data checkbox
     logging.info('check purge data checkbox (waiting)')
     purge_check = driver.find_element(By.ID, 'purge')
-    # purge_check = WebDriverWait(driver, 60).until(expected_conditions.visibility_of_element_located((By.ID, 'purge')))
     driver.execute_script("arguments[0].click();", purge_check)
     # purge_check.click()
     logging.info('Purge Checked')
@@ -119,7 +118,7 @@ def delete_gtd(url):
     # click on delete button
     logging.info('Delete data')
     driver.find_element(By.CLASS_NAME, 'form-horizontal').submit()
-    logging.info(f'Sucessfully deleted GDT {url}')
+    logging.info(f'Sucessfully deleted GTD {url}')
 
 def create_new_gtd(key, name, short_name):
     """ create new globalstats tracked data """
@@ -144,36 +143,36 @@ def create_new_gtd(key, name, short_name):
     short_name_input.send_keys(short_name)
     time.sleep(.200)
 
-    logging.info("Submit - Create new GDT ")
+    logging.info("Submit - Create new GTD ")
     driver.find_element(By.CLASS_NAME, 'form-horizontal').submit()
-    logging.info(f'Sucessfully created new GDT for {name}')
+    logging.info(f'Sucessfully created new GTD for {name}')
     
 
 if __name__ == '__main__':
-    if gdt == 'weekly':
+    if gtd == 'weekly':
         try:
             sign_in()
             delete_gtd(weekly_dlt_url)
             time.sleep(.600)
-            create_new_gtd(key=weekly_gdt_key, name=weekly_gdt_key, short_name=weekly_gdt_short_name)
+            create_new_gtd(key=weekly_gtd_key, name=weekly_gtd_key, short_name=weekly_gtd_short_name)
         except Exception as e:
             logging.exception(e)
     
-    if gdt == 'monthly':
+    if gtd == 'monthly':
         try:
             sign_in()
             delete_gtd(monthly_dlt_url)
             time.sleep(.600)
-            create_new_gtd(key=monthly_gdt_key, name=monthly_gdt_key, short_name=monthly_gdt_short_name)
+            create_new_gtd(key=monthly_gtd_key, name=monthly_gtd_key, short_name=monthly_gtd_short_name)
         except Exception as e:
             logging.exception(e)
 
-    if gdt == 'test':
+    if gtd == 'test':
         try:
             sign_in()
             delete_gtd(test_gdt_dlt_url)
             time.sleep(.600)
-            create_new_gtd(key=test_gdt_key, name=test_gdt_key, short_name=test_gdt_short_name)
+            create_new_gtd(key=test_gtd_key, name=test_gtd_key, short_name=test_gtd_short_name)
         except Exception as e:
             logging.exception(e)
     
